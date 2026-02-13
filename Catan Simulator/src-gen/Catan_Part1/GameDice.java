a95820b3f64912ec04aa1bfa6d2f3cf6e52f8177
erjonak <kalarie@mcmaster.ca>
2026-02-12 19:11:56 -0500

    made Dice interface

---
+++ Catan Simulator/src-gen/Catan_Part1/GameDice.java
@@ -0,0 +1,20 @@
+package Catan_Part1;
+
+import java.util.Random;
+
+public class GameDice implements Dice{
+
+    private Random random;
+
+    public Dice(Random random){
+        this.random = random;
+    }
+
+    @Override
+    public int roll(){
+        int die1 = random.nextInt(6) + 1; 
+        int die2 = random.nextInt(6) + 1; 
+        return die1 + die2; 
+    }
+
+}
