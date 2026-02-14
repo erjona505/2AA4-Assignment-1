8200412bee62676b506a340ef5fdf7acc3c123d2
erjonak <kalarie@mcmaster.ca>
2026-02-13 21:43:51 -0500

    adding comments

--- Catan Simulator/src-gen/Catan_Part1/Dice.java
+++ Catan Simulator/src-gen/Catan_Part1/Dice.java
@@ -1,6 +1,14 @@
 package Catan_Part1;
-
+/**
+ * Represents a dice mechanism used in the game.
+ * 
+ * Any class implementing this interface must define how a dice
+ * roll is generated.
+ * 
+ * @author Erjona Kalari
+ */
 public interface Dice {
+    
     
     int roll();
 
--- Catan Simulator/src-gen/Catan_Part1/GameDice.java
+++ Catan Simulator/src-gen/Catan_Part1/GameDice.java
@@ -1,15 +1,27 @@
 package Catan_Part1;
-
+/**
+ * 
+ * This class implements the Dice interface, which defines a method for rolling the dice.
+ * The roll() method simulates rolling two six-sided dice and returns their sum.
+ * 
+ * @author Erjona Kalari
+ */
 import java.util.Random;
 
 public class GameDice implements Dice{
 
+    //random number generator used to simulate dice rolls
     private Random random;
 
     public GameDice(Random random){
         this.random = random;
     }
 
+     /**
+     * Simulates rolling two six-sided dice.
+     *
+     * @return the sum of the two dice rolls
+     */
     @Override
     public int roll(){
         int die1 = random.nextInt(6) + 1; 
