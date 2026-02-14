a2e69c1622f53e1847c444d09616be5093e03b2d
ZainKhalbous <zainkhalbous@gmail.com>
2026-02-13 22:48:15 -0500

    Refactor ResourceType enum by removing comments
    Removed unnecessary comments and cleaned up the enum definition.

--- Catan Simulator/src-gen/Catan_Part1/ResourceType.java
+++ Catan Simulator/src-gen/Catan_Part1/ResourceType.java
@@ -1,33 +1,21 @@
 package Catan_Part1;
 
-/************************************************************/
-/**
+/**enum class
+ * has the resources types
  *
- */
+ * **/
 public enum ResourceType {
-    /**
-     *
-     */
+
     WOOD,
-    /**
-     *
-     */
+
     WHEAT,
-    /**
-     *
-     */
+
     ORE,
-    /**
-     *
-     */
+
     SHEEP,
-    /**
-     *
-     */
+
     BRICK,
-    /**
-     *
-     */
+
     DESERT
     ;
 }
