e903e156e254c80d08df50b4e00dd5500a62eb42
Zain Sakaji <zain.sakaji@gmail.com>
2026-03-18 18:00:52 -0400

    added R3.2

---
+++ Catan Simulator/src-gen/Catan_Part3/BuildCityRule.java
@@ -0,0 +1,14 @@
+package Catan_Part3;
+
+public class BuildCityRule implements Rule{
+    @Override
+    public double evaluate(Agent agent, GameMap map) {
+        if (!agent.canBuildCity(map)) return 0;
+        return 1;  //city gives VP
+    }
+
+    @Override
+    public boolean apply(Agent agent, GameMap map) {
+        return agent.tryBuildCity(map);
+    }
+}
