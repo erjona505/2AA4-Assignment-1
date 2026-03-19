7fecee8ea52a4726b50cde80f12325ba5798f8e7
erjona505 <erjona.kalari@gmail.com>
2026-03-19 13:12:25 -0400

    adding comments

--- Catan Simulator/src-gen/Catan_Part3/VisualizerAdapter.java
+++ Catan Simulator/src-gen/Catan_Part3/VisualizerAdapter.java
@@ -1,22 +1,31 @@
 package Catan_Part3;
 
 /**
- * This class applies the Adapter design pattern that bridges the Visualizer interface.
+ * This class represents the Adapter in the Adapter Pattern.
+ *
+ * It connects Visualizer interface to the GameState class and translates
+ * calls from update(Game) into export(Game), allowing the Game
+ * class to use the visualizer without knowing about JSON or file-writing details.
  */
 
 public class VisualizerAdapter implements Visualizer {
+
     private final GameState state;
 
+    //initializes the adaptee (GameState) with a file path
     public VisualizerAdapter(String filePath) {
         this.state = new GameState(filePath);
 
     }
 
+    //implements the Visualizer interface method
     @Override
     public void update(Game game) {
-        state.export(game);
+
+        state.export(game); //delegate the call to adaptee
     }
 
+    //closes visualizer
     @Override
     public void close(){
         System.out.println("Visualizer closed.");
